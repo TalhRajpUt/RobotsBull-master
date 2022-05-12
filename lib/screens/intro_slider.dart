840bcfac26d1c2b32b50b5b20a822ba98816cda2
TalhaRajpUt <talharajput1406@gmail.com>
2022-05-12 19:30:55 +0500

    .

--- lib/screens/intro_slider.dart
+++ lib/screens/intro_slider.dart
@@ -2,7 +2,6 @@
 import 'package:app/screens/webview_screen.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -19,163 +18,192 @@ class _IntroSliderState extends State<IntroSlider> {
   Widget build(BuildContext context) {
     double _height = MediaQuery.of(context).size.height;
     double _width = MediaQuery.of(context).size.width;
-    var _padding = MediaQuery.of(context).padding;
-    double newHeight = _height - _padding.top;
     return Scaffold(
-      body: Stack(
+        body: SafeArea(
+      top: false,
+      child: Column(
         children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              onPageChanged: ((index, reason) {
-                setState(() {
-                  _current = index;
-                });
-              }),
-              enableInfiniteScroll: false,
-              viewportFraction: 1,
-              height: newHeight * 1,
-              autoPlay: false,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: false,
-              padEnds: true,
-            ),
-            items: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/images/1.jpeg',
-                    fit: BoxFit.fill,
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Stack(
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                    onPageChanged: ((index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    }),
+                    enableInfiniteScroll: false,
+                    viewportFraction: 1,
+                    height: _height * 0.9,
+                    autoPlay: false,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: false,
+                    padEnds: true,
                   ),
-                  SizedBox(height: newHeight * 0.06),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.07, right: _width * 0.07),
-                    child: Text('Welcome to RobotBulls',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500)),
-                  ),
-                  SizedBox(height: newHeight * 0.03),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.07, right: _width * 0.07),
-                    child: Center(
-                      child: Text(
-                        "We develop trading robots that research for you on the basis of past market experiences and of traders' current emotions.",
-                        softWrap: true,
-                        style: TextStyle(
-                            color: Color.fromARGB(221, 1, 1, 1),
-                            fontSize: 21,
-                            fontWeight: FontWeight.w200),
-                      ),
+                  items: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/1.jpeg',
+                          fit: BoxFit.fill,
+                        ),
+                        SizedBox(height: _height * 0.06),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.07, right: _width * 0.07),
+                          child: Text('Welcome to RobotBulls',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(height: _height * 0.03),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.07, right: _width * 0.07),
+                          child: Center(
+                            child: Text(
+                              "We develop trading robots that research for you on the basis of past market experiences and of traders' current emotions.",
+                              softWrap: true,
+                              style: TextStyle(
+                                  color: Color.fromARGB(221, 1, 1, 1),
+                                  fontSize: 21,
+                                  fontWeight: FontWeight.w200),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/images/two.jpeg',
-                    fit: BoxFit.fill,
-                  ),
-                  SizedBox(height: newHeight * 0.06),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.07, right: _width * 0.07),
-                    child: Text('Invest with ease',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500)),
-                  ),
-                  SizedBox(height: newHeight * 0.03),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.05, right: _width * 0.05),
-                    child: Text(
-                      "Choose your trading bot and invest your favourite asset.",
-                      softWrap: true,
-                      style: TextStyle(
-                          color: Color.fromARGB(221, 1, 1, 1),
-                          fontSize: 21,
-                          fontWeight: FontWeight.w200),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/two.jpeg',
+                          fit: BoxFit.fill,
+                        ),
+                        SizedBox(height: _height * 0.06),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.07, right: _width * 0.07),
+                          child: Text('Invest with ease',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(height: _height * 0.03),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.05, right: _width * 0.05),
+                          child: Text(
+                            "Choose your trading bot and invest your favourite asset.",
+                            softWrap: true,
+                            style: TextStyle(
+                                color: Color.fromARGB(221, 1, 1, 1),
+                                fontSize: 21,
+                                fontWeight: FontWeight.w200),
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/images/three.jpeg',
-                    fit: BoxFit.fill,
-                  ),
-                  SizedBox(height: newHeight * 0.06),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.07, right: _width * 0.07),
-                    child: Text('Unsure about your investment? Try the Demo',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500)),
-                  ),
-                  SizedBox(height: newHeight * 0.03),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: _width * 0.05, right: _width * 0.05),
-                    child: Text(
-                      "We offer users the possibility to invest in all of our trading bots with fake money in order to see over some days/weeks/months how the investment evolves",
-                      softWrap: true,
-                      style: TextStyle(
-                          color: Color.fromARGB(221, 1, 1, 1),
-                          fontSize: 21,
-                          fontWeight: FontWeight.w200),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          SizedBox(height: newHeight * 0.7),
-          buildIndicator(),
-          SizedBox(height: newHeight * 0.2),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                  fixedSize: Size(MediaQuery.of(context).size.width * 0.8,
-                      MediaQuery.of(context).size.height * 0.06),
-                  backgroundColor: Colors.blue),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WebviewScreen()),
-                );
-              },
-              child: Text('Get Started',
-                  style: GoogleFonts.roboto(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white)),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/three.jpeg',
+                          fit: BoxFit.fill,
+                        ),
+                        SizedBox(height: _height * 0.06),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.07, right: _width * 0.07),
+                          child: Text(
+                              'Unsure about your investment? Try the Demo',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(height: _height * 0.03),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: _width * 0.05, right: _width * 0.05),
+                          child: Text(
+                            "We offer users the possibility to invest in all of our trading bots with fake money in order to see over some days/weeks/months how the investment evolves",
+                            softWrap: true,
+                            style: TextStyle(
+                                color: Color.fromARGB(221, 1, 1, 1),
+                                fontSize: 21,
+                                fontWeight: FontWeight.w200),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Positioned(
+                  top: _height * 0.43,
+                  left: _width * 0.4,
+                  right: _width * 0.4,
+                  child: buildIndicator(),
+                )
+              ],
             ),
           ),
+          Center(
+            child: InkWell(
+              child: Container(
+                alignment: Alignment.center,
+                height: _height * 0.06,
+                width: _width * 0.8,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue),
+                child: Text('Get Started',
+                    style: GoogleFonts.roboto(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white)),
+              ),
+            ),
+          )
         ],
       ),
-    );
+    ));
   }
 
   Widget buildIndicator() => AnimatedSmoothIndicator(
         activeIndex: _current,
         count: 3,
         effect: SlideEffect(
-            dotHeight: 8,
-            dotWidth: 8,
+            dotHeight: 10,
+            dotWidth: 10,
             activeDotColor: Colors.white,
             dotColor: Colors.grey),
       );
 }
+
+
+// ClipRRect(
+//           borderRadius: BorderRadius.circular(10),
+//           child: OutlinedButton(
+//             style: OutlinedButton.styleFrom(
+//                 fixedSize: Size(MediaQuery.of(context).size.width * 0.8,
+//                     MediaQuery.of(context).size.height * 0.06),
+//                 backgroundColor: Colors.blue),
+//             onPressed: () {
+//               Navigator.pushReplacement(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => const WebviewScreen()),
+//               );
+//             },
+//             child: Text('Get Started',
+//                 style: GoogleFonts.roboto(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.w600,
+//                     color: Colors.white)),
+//           ),
+//         ),
